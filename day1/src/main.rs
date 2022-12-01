3b2e76e4593044f673de45292f0e80ef58556a83
philip-nielsen <philipnielsen@gmail.com>
2022-12-01 14:16:59 +0100

    prettier

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,25 +3,19 @@ use std::io::{ BufReader, BufRead, Error};
 
 fn main() -> Result<(), Error> {
     let path = "input.txt";
-
-    //let mut output = File::create(path)?;
-
     let input = File::open(path)?;
     let buffered = BufReader::new(input);
-
+    
     let mut biggest_sum: i32 = 0;
     let mut second_sum: i32 = 0;
     let mut third_sum: i32 = 0;
     let mut temp_sum: i32 = 0;
 
     for line in buffered.lines() {
-        //println!("{}", line?);
         let x = line?;
         if x != "" {
             let num: i32 = x.parse().unwrap();
-
             temp_sum += num;
-            //println!("{}", num);
         } else {
             if temp_sum > biggest_sum {
                 third_sum = second_sum;
@@ -37,12 +31,6 @@ fn main() -> Result<(), Error> {
             temp_sum = 0;
         }
     }
-
-    //println!("{}", biggest_sum);
-    //println!("{}", second_sum);
-    //println!("{}", third_sum);
-
-
     let sum: i32 = biggest_sum + second_sum + third_sum;
     print!("{}", "answer part 1:");
     println!("{}", biggest_sum);
