baab945b2bf02a1d343bba89849643e468881cce
Sea_Raider <philipnielsen64@gmail.com>
2022-12-04 19:34:46 +0100

    day4, not working :(

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -6,11 +6,31 @@ fn main() -> Result<(), Error> {
     let input = File::open(path)?;
     let buffered = BufReader::new(input);
     let mut score = 0;
-    
+
     for line in buffered.lines() {
         let x = line?;
-        let char_vec: Vec<char> = x.chars().collect();
+        let section_ranges = x.split(",");
+        let mut low = 0;
+        let mut high = 1000;
+        let mut last_low = 100000;
+        let mut last_high = -1000;
+
+        for section_range in section_ranges {
+            let range = section_range.split("-");
+            high = range.clone().last().unwrap().parse().unwrap();
+            low = range.min().unwrap().parse().unwrap();
+
+            if last_low == 100000 {
+                last_high = high;
+                last_low = low;
+            }
+
+
+        }
         
+        if (high >= last_high && low <= last_low) || (last_high >= high && last_low <= low){
+            score += 1;
+        }
     }
 
     println!("{}", score);
