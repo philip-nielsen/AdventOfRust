6438252a46cfcbee669e6a6301dae59d21a6260f
Sea_Raider <philipnielsen64@gmail.com>
2022-12-06 21:41:21 +0100

    part1

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,3 +1,40 @@
+use std::fs::File;
+use std::collections::HashSet;
+
 fn main() {
-    println!("Hello, world!");
+    let path = "input.txt";
+    let input = include_str!("../input.txt");
+    let mut score = 1;
+    let mut first = ' ';
+    let mut second = ' ';
+    let mut third = ' ';
+
+    for c in input.chars() {
+        let mut singals:HashSet<char> = HashSet::new();
+        let temp1 = first.clone();
+        let temp2 = second.clone();
+        let temp3 = third.clone();
+        
+        match score {
+            1 => first = c,
+            2 => second = c,
+            3 => third = c,
+            _ => {
+                singals.insert(temp1);
+                singals.insert(temp2);
+                singals.insert(temp3);
+                singals.insert(c);
+                if singals.len() == 4 {
+                    break;
+                } else {
+                    first = second;
+                    second = third;
+                    third = c;
+                }
+            }
+        };
+        score += 1;
+        
+    }
+    println!("{}",score);
 }
